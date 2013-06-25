0770d460db36dd7714d641930244cf61cb3ae3ca
Jimmy <admin@jimmymadon.com>
2013-06-25 12:30:03 +0100

    Completed configuring BoostCake Plugin.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -36,4 +36,11 @@ class AppController extends Controller {
 	public $components = array(
 		'DebugKit.Toolbar'
 	);
+
+	public $helpers = array(
+		'Session',
+		'Html' => array('className' => 'BoostCake.BoostCakeHtml'),
+		'Form' => array('className' => 'BoostCake.BoostCakeForm'),
+		'Paginator' => array('className' => 'BoostCake.BoostCakePaginator'),
+	);
 }
